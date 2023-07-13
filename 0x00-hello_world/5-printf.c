ef347abf6efec2b1ac1019ff55454e43ce604a3c
ogwsaroca <adarfidel.ogw@gmail.com>
2023-07-13 09:11:52 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: always 0 (success)
+ * main - Program printing various sizes of computer types
+ * Return: 0 
  */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
-}
+return (0); }
