afa273f2788be67c94267636dc38682252fdac1b
ogwsaroca <adarfidel.ogw@gmail.com>
2023-07-13 09:50:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <studio.h>
 /**
- * main - Program printing various sizes of computer types
- * Return: 0 
+ * main - A program that prints a line using the printf function
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0); }
+printf("with proper grammar, but the outcome is a piece of art,"\n
+return (0);
+}
