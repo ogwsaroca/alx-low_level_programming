0d5534bbb677a125089a8f6e4a0209408fe9f72c
ogwsaroca <adarfidel.ogw@gmail.com>
2023-07-21 02:07:47 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,11 @@
  * print_numbers - Print the numbers since 0 up to 9
  * Return: The numbers 0 up to 9
  */
- void print_numbers(void)
+void print_numbers(void)
 {
 	int x;
 
-	for (x = 0; x<=9; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		_putchar(x + '0');
 	}
