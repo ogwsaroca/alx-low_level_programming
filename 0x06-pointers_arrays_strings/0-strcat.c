ab181d6ed33dbc7063a11292d12fbf3fa89c6974
ogwsaroca <adarfidel.ogw@gmail.com>
2023-07-27 02:28:13 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strncat - Concatenates wo strings using at most
- * an inputted number of bytes from src
- * @dest: The string to be appended upon
- * @src: the string to be appended to dest
- * @n: The number of bytes from src to be appended to dest
+ * _strcat - Concentrates on two strings
+ * @dest: The destination string
+ * @src: The source string
  * Return: A pointer to the returning string dest
  */
 
-char *_srncat(char *dest, char *src, int n)
+char _strcat(char *dest, char *src)
 
 {
-	int index = 0, dest_len = 0;
+	int dlen = 0, i;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	while (dest[dlen])
+
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies at most an inputed number
- * of bytes from string src into dest.
- * @dest: The buffer storing the string copy
- * @src: The source string
- * @n: The maximum number of strings to be copied from src
- * Return: A pointer to the resulting string dest
+ * _strncat - Concatenates wo strings using at most
+ * an inputted number of bytes from src
+ * @dest: The string to be appended upon
+ * @src: the string to be appended to dest
+ * @n: The number of bytes from src to be appended to dest
+ * Return: A pointer to the returning string dest
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_srncat(char *dest, char *src, int n)
 
 {
-	int index = 0; src_len = 0;
+int index = 0, dest_len = 0;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
