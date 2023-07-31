b1509854cdabc42d7b42b52fc45352aaeceab864
ogwsaroca <adarfidel.ogw@gmail.com>
2023-07-31 10:31:15 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@ char *_memeset(char *s, char b, unsigned int n);
 char *_memecpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *strpbrk(char *s, char *accept);
+const char *(char *s, char *accept);
 char *strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[0]);
 void print_diagsums(int *a, int size);
